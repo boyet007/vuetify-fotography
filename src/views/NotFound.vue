16994d54973b562e8bcd571735af955ec950862f
Philip Loa <boyet007@gmail.com>
2020-08-21 22:09:23 +0700

    Vuetify #11 - Working on Not Found Page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,11 @@
 <template>
-    <v-main>
-        Not Found
-    </v-main>
+    <v-container>
+        <div class="block py-10 text-center">
+            <h1>Page Not Found</h1>
+            <i class="fa fa-exclamation-triangle red--text py-15"></i>
+            <p>We are sorry the page your request could not be found.</p>
+        </div>
+    </v-container>
 </template>
 
 <script>
